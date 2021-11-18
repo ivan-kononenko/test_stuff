98560b29e82db0ca4fd0923aeefbec616a884a42
ivan-kononenko <ivan.a.kononenko@gmail.com>
2021-11-18 21:01:12 +0200

    fix

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import string
 import re
 
 abc = string.ascii_lowercase + string.ascii_uppercase
-vector_shift = 1 #This shift is required to compensate for differences in counting - humans start from 1, while computer starts from 0.
+vector_shift = 1 #This shift is required to compensate for differences in counting - 0 is an empty space, while 1 is an "a" symbol.
 
 def vectorizer(text):
     text_transliterized = translit(text, UkrainianKMU)
@@ -14,19 +14,15 @@ def vectorizer(text):
         sentence_vector = []
         for word in words:
             if word != " ":
-                word_vector = 0
+                word_vector = 0 
                 for letter in word:
-                   word_vector = word_vector + abc.index(letter) + vector_shift
-            sentence_vector.append(word_vector)
+                    word_vector = word_vector + abc.index(letter) + vector_shift
+                sentence_vector.append(word_vector)
         text_vector.append(sentence_vector)
 
 
 
-            #print(word_vector)
-            #print("break")
-        #print(sentence_vector)
-        #print("break 2")
     print(text_vector) 
-    #print("break 3")
+    return(text_vector)
 text = "Тепер шафа зі спеціями пахне нашою суботою. Батько наш Бандера! Чомусь, але чому? Апостроф, м'який знак, гречка?!"
 vectorizer(text)
